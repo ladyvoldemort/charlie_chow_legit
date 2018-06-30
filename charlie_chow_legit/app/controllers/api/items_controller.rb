6fc098a110953e8c48276ce8f40b70a0d63f71ce
Brittany V <brittbyu@gmail.com>
2018-06-30 11:03:17 -0600

    back on track! update!

---
+++ charlie_chow_legit/app/controllers/api/items_controller.rb
@@ -0,0 +1,38 @@
+class Api::ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :update, :destroy]
+
+    def index
+      render json: Item.all
+    end
+
+    def create
+      item = Item.new(item_params)
+      if item.save
+        render json: item
+      else
+        render json: { errors: item.errors }, status: :unprocessable_entity 
+      end
+    end
+
+    def update
+      item = Item.find(params[:id])
+      item.update(complete: !item.complete)
+      render json: item
+    end
+
+    def destroy
+      Item.find(params[:id]).destroy
+      render json: { message: 'Item deleted' }
+    end
+
+    private
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:name, :complete)
+    end
+
+end
