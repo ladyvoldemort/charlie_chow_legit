8ce31ab32ca61042e9fde3d99c156bf79867f312
Deneb Pulsipher <pulsipherd@gmail.com>
2018-06-30 12:15:43 -0600

    fix seed file

--- charlie_chow_legit/db/seeds.rb
+++ charlie_chow_legit/db/seeds.rb
@@ -15,56 +15,56 @@ Item.create(
 
 Item.create(
             name: "Chream Cheese Wontons",
-            price:5.50
+            price:5.50,
             spicy:false)
 
 Item.create(
             name: "Stuffed Wontons",
-            price:4.99
+            price:4.99,
             spicy:false)
 
 Item.create(
             name: "Szechuan Dumplings",
-            price:4.55
+            price:4.55,
             spicy:true)
 
 Item.create(
             name: "Potstickers",
-            price:4.55
+            price:4.55,
             spicy:false)
 
 Item.create(
             name: "Vegetable Pot Stickers",
-            price:4.55
+            price:4.55,
             spicy:false)
 
 Item.create(
             name: "Char Siu Bao",
-            price:4.75
+            price:4.75,
             spicy:false)
 
 Item.create(
             name: "Fantail Shrimp",
-            price:4.99
+            price:4.99,
             spicy:false)
 
 Item.create(
             name: "BBQ Sliced Pork (Char Siu)",
-            price:5.95
+            price:5.95,
             spicy:false)
 
 Item.create(
             name: "Chicken Fingers",
-            price:7.25
+            price:7.25,
             spicy:false)
 
 Item.create(
             name: "Assorted Dim Sum",
-            price:7.99
+            price:7.99,
             spicy:false)
 
 Item.create(
             name: "Dim Sum Feast",
-            price:12.99
+            price:12.99,
             spicy:false)
 
