209f52415e6faf8af0300fc79814eb007b9f037e
Joanna Stronska <stronska.j@gmail.com>
2018-06-30 15:52:29 -0600

    removed some routes

--- charlie_chow_legit/config/routes.rb
+++ charlie_chow_legit/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'shopping_cart/has_many:'
-  get 'shopping_cart/items'
+ 
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
     #API ROUTES SHOULD GO HERE
