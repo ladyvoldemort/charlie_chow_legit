a388cbce0c0275de9166390ce627f33b4c7800d4
Deneb Pulsipher <pulsipherd@gmail.com>
2018-06-30 11:58:57 -0600

    add seed file

--- charlie_chow_legit/db/seeds.rb
+++ charlie_chow_legit/db/seeds.rb
@@ -1,7 +1,70 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Item.create(
+            name: "Cantonese Spring Rolls",
+            price: 3.75,
+            spicy: false)
+
+Item.create(
+            name: "Crispy Eggplant with Cheese",
+            price: 4.24,
+            spicy: false)
+
+Item.create(
+            name: "Butterfly Shrimp",
+            price: 5.95,
+            spicy: false)
+
+Item.create(
+            name: "Chream Cheese Wontons",
+            price:5.50
+            spicy:false)
+
+Item.create(
+            name: "Stuffed Wontons",
+            price:4.99
+            spicy:false)
+
+Item.create(
+            name: "Szechuan Dumplings",
+            price:4.55
+            spicy:true)
+
+Item.create(
+            name: "Potstickers",
+            price:4.55
+            spicy:false)
+
+Item.create(
+            name: "Vegetable Pot Stickers",
+            price:4.55
+            spicy:false)
+
+Item.create(
+            name: "Char Siu Bao",
+            price:4.75
+            spicy:false)
+
+Item.create(
+            name: "Fantail Shrimp",
+            price:4.99
+            spicy:false)
+
+Item.create(
+            name: "BBQ Sliced Pork (Char Siu)",
+            price:5.95
+            spicy:false)
+
+Item.create(
+            name: "Chicken Fingers",
+            price:7.25
+            spicy:false)
+
+Item.create(
+            name: "Assorted Dim Sum",
+            price:7.99
+            spicy:false)
+
+Item.create(
+            name: "Dim Sum Feast",
+            price:12.99
+            spicy:false)
+
